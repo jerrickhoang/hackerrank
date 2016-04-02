cb00fd7bb61fbf597c5bbab5bd38d3dad1ee1ea8
Jerrick <ahoang@tellapart.com>
2016-04-01 23:48:02 -0700

    Algo.Strings.MakeItAnagram

---
+++ algorithms/strings/make_it_anagram.cpp
@@ -0,0 +1,56 @@
+/*Alice recently started learning about cryptography and found that anagrams are very useful. Two strings are anagrams of each other if they have same character set (and frequency of characters) and same length. For example strings "bacdc" and "dcbac" are anagrams, while strings "bacdc" and "dcbad" are not.
+
+Alice decides on an encryption scheme involving 2 large strings where encryption is dependent on the minimum number of character deletions required to make the two strings anagrams. She need your help in finding out this number.
+
+Given two strings (they can be of same or different length) help her in finding out the minimum number of character deletions required to make two strings anagrams. Any characters can be deleted from any of the strings.
+
+Input Format 
+Two lines each containing a string.
+
+Constraints 
+1 <= Length of A,B <= 10000 
+A and B will only consist of lowercase latin letter.
+
+Output Format 
+A single integer which is the number of character deletions.
+
+Sample Input #00:
+
+cde
+abc
+Sample Output #00:
+
+4
+Explanation #00: 
+We need to delete 4 characters to make both strings anagram i.e. 'd' and 'e' from first string and 'b' and 'a' from second string.
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    string s1, s2;
+    cin >> s1;
+    cin >> s2;
+    int n1, n2, res =0;
+    int t1[26]={}, t2[26]={};
+    n1 = s1.size();
+    n2 = s2.size();
+    for (int i = 0; i < n1; i ++) {
+        t1[s1[i]-97]++;
+    }
+    for (int i = 0; i < n2; i ++) {
+        t2[s2[i]-97]++;
+    }
+    for (int i = 0; i < 26; i ++) {
+        res += abs(t1[i] - t2[i]);
+    }
+    cout << res << endl;
+}
+
